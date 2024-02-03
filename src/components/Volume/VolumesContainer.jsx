da727f8ee95fe3409d4295c5369a2d257200d75b
hanna <hanna.chegini@gmail.com>
2024-02-04 00:41:19 +0330

    Add Volume data

--- src/components/Volume/VolumesContainer.jsx
+++ src/components/Volume/VolumesContainer.jsx
@@ -9,14 +9,29 @@ import axios from 'axios';
 function VolumesContainer() {
     // Define the volume data for each row
     // const volumesData = [
-    //     { number: 1, title: 'Volume 1', journalName: 'Journal 1', description: 'Description 1', releaseDate: '2022-01-01', img: 'image1.jpg' },
-    //     { number: 2, title: 'Volume 2', journalName: 'Journal 2', description: 'Description 2', releaseDate: '2022-02-01', img: 'image2.jpg' },
-    //     // Add more volume data as needed
+    // { number: 43, title: 'سنت و مدرنیته : میراث و نوآوری', journalName: 'بارقه',
+    //     description: 'Description 1', releaseDate: '2022-01-01',
+    //     img : process.env.PUBLIC_URL + "/data/bareghe43.jpeg", fileUrl : process.env.PUBLIC_URL + "/data/bareghe43.pdf" },
+    // { number: 1, title: 'Lovely Bits Get Together', journalName: 'بایت',
+    //     description: 'Description 2', releaseDate: '2022-02-01',
+    //     img: process.env.PUBLIC_URL + "/data/byte1.jpeg" , fileUrl : process.env.PUBLIC_URL + "/data/byte1.pdf"  },
+    // { number: 16, title: 'ویژه نامه ورودی ها', journalName: 'رایانش',
+    //     description: 'دانشگاه آزادی! دانشگاه آزامش! تاثیرپذیرتر از آنچه فکر میکنید', releaseDate: '2022-01-01',
+    //     img : process.env.PUBLIC_URL + "/data/Rayanesh16.jpeg", fileUrl : process.env.PUBLIC_URL + "/data/Rayanesh16.pdf" },
+    // { number: 14, title: 'چهارده!', journalName: 'رایانش',
+    //     description: 'قرنطینه، دل‌تنگی، مرثیه، گراف، دوستی، تغییر، سفر، بالش،‌ پتو، پایتون، تمرین، رویداد، بازی، رایانش، تمدید ...', releaseDate: '2022-01-01',
+    //     img : process.env.PUBLIC_URL + "/data/Rayanesh14.jpeg", fileUrl : process.env.PUBLIC_URL + "/data/Rayanesh14.pdf" },
+    // { number: 15, title: 'اولین شماره قرن', journalName: 'رایانش',
+    //     description: 'به هنگام رفتن، به وقت دلتنگی', releaseDate: '2022-01-01',
+    //     img : process.env.PUBLIC_URL + "/data/rayanesh15.jpeg", fileUrl : process.env.PUBLIC_URL + "/data/Rayanesh15.pdf" },
+    //
     // ];
     const [volumesData, setVolumesData] = useState([]);
     useEffect(() => {
         fetchVolumes();
     }, []);
+
+
 
     const fetchVolumes = async () => {
         try {
@@ -26,7 +41,7 @@ function VolumesContainer() {
             console.error(error);
         }
     };
-    
+
     return (
         <Container maxWidth = 'lg' className = 'Container'>
             <Grid
