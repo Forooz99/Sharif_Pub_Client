6b3b57820078340f14f39c6d72a14506264d033d
hanna <hanna.chegini@gmail.com>
2024-02-03 11:17:35 +0330

    Update appearance off volume

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import './Home.css';
-import VolumeCards from '../Volume/VolumeCards'; // Import the VolumeCards component
+import VolumesContainer from '../Volume/VolumesContainer'; // Import the VolumeCards component
 
 const Home = () => {
-  // Define the volume data for each row
-  const volumesData = [
-    { number: 1, title: 'Volume 1', journalName: 'Journal 1', description: 'Description 1', releaseDate: '2022-01-01', img: 'image1.jpg' },
-    { number: 2, title: 'Volume 2', journalName: 'Journal 2', description: 'Description 2', releaseDate: '2022-02-01', img: 'image2.jpg' },
-    // Add more volume data as needed
-  ];
 
   return (
     <div className="centered-container">
-      <div className="volumes-grid">
-        {/* Render four rows, each containing two volumes */}
-        {[0, 1, 2, 3].map((index) => (
-          <div key={index} className="row">
-            <VolumeCards {...volumesData[index * 2]} /> {/* Render first volume in the row */}
-            <VolumeCards {...volumesData[index * 2 + 1]} /> {/* Render second volume in the row */}
-          </div>
-        ))}
-      </div>
+        <VolumesContainer/>
     </div>
   );
 };
--- src/components/Volume/VolumesContainer.jsx
+++ src/components/Volume/VolumesContainer.jsx
@@ -2,8 +2,15 @@ import * as React from 'react';
 import "./Volume.css";
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import VolumeCards from "./VolumeCards";
 
 function VolumesContainer() {
+    // Define the volume data for each row
+    const volumesData = [
+        { number: 1, title: 'Volume 1', journalName: 'Journal 1', description: 'Description 1', releaseDate: '2022-01-01', img: 'image1.jpg' },
+        { number: 2, title: 'Volume 2', journalName: 'Journal 2', description: 'Description 2', releaseDate: '2022-02-01', img: 'image2.jpg' },
+        // Add more volume data as needed
+    ];
 
     return (
         <Container maxWidth = 'lg' className = 'Container'>
@@ -13,8 +20,11 @@ function VolumesContainer() {
                 direction="row"
                 spacing={4}
                 style={{ marginTop: 15 }}>
-
-
+                {volumesData.map((volume) => (
+                    <Grid item xs={6} key={volume.number}>
+                        <VolumeCards {...volume} />
+                    </Grid>
+                ))}
             </Grid>
         </Container>
     );
